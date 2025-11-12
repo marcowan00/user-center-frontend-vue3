3112eea74c51ce70ee077c85235d61ab565bbf33
Marco WAN <marcowan00@gmail.com>
2025-11-12 20:22:33 +0800

    使用插件配置pinia store持久化，将用户登录状态保存在本地，解决了登录状态下，在用户管理页面刷新会被路由守卫拦截的问题

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import App from "./App.vue";
 import "./style.css";
 import router from "./router";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-const pinia = createPinia();
 const app = createApp(App);
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
 app.use(Antd);
--- src/stores/useLoginUserStore.ts
+++ src/stores/useLoginUserStore.ts
@@ -2,31 +2,30 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { getCurrentUser } from "../services/user";
 
-const useLoginUserStore = defineStore("loginUser", () => {
-  const loginUser = ref<any>({
-    username: "未登录",
-  });
+const useLoginUserStore = defineStore(
+  "loginUser",
+  () => {
+    const loginUser = ref<any>({
+      username: "未登录",
+    });
 
-  // 从后端获取当前登录用户信息
-  const fetchLoginUser = async () => {
-    const res = await getCurrentUser();
+    // 从后端获取当前登录用户信息
+    const fetchLoginUser = async () => {
+      const res = await getCurrentUser();
 
-    if (res.data.code === 0 && res.data.data) {
-      loginUser.value = res.data.data;
-    }
-    // else {
-    //   setTimeout(() => {
-    //     loginUser.value = { username: "marcowan00", id: 1 };
-    //   }, 2_000);
-    // }
-  };
+      if (res.data.code === 0 && res.data.data) {
+        loginUser.value = res.data.data;
+      }
+    };
 
-  // 直接设置登录用户信息
-  const setLoginUser = (newLoginUser: any) => {
-    loginUser.value = newLoginUser;
-  };
+    // 直接设置登录用户信息
+    const setLoginUser = (newLoginUser: any) => {
+      loginUser.value = newLoginUser;
+    };
 
-  return { loginUser, fetchLoginUser, setLoginUser };
-});
+    return { loginUser, fetchLoginUser, setLoginUser };
+  },
+  { persist: true }
+);
 
 export default useLoginUserStore;
